e915f7b14e2eb0e843c86910b9c0cad83e0d50d1
Parbat Lama <lamaparbat70@gmail.com>
2022-06-05 16:57:26 +0545

    Completed the image and container configuration

---
+++ index.js
@@ -0,0 +1,23 @@
+//importing modules
+const express = require("express");
+const cors = require("cors");
+
+//config
+const PORT = process.env.PORT || 3000;
+
+//instance
+const server = express();
+
+//middleware
+server.use(cors());
+
+//routes
+server.get("/", (req, res) => {
+ console.log("Server has now started... !!");
+ res.status(200).send("Server has now started !!")
+});
+
+//listener
+server.listen(PORT, () => {
+ console.log(`Listening to the server on PORT ${PORT}`);
+})
